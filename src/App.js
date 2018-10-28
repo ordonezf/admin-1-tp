62a9e3ae8f9f55b80ba419c33870ef898c131e16
emariotti3 <eugeniamariotti93@gmail.com>
2018-10-28 15:54:58 -0300

    Added login/register stylesheet and basic layout form.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+// eslint-disable
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App__Aside"></div>
+        <div className="App__Form">
+            <div className="PageSwitcher">
+              <a href="#" className="PageSwitcher__Item">Sign In</a>
+              <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
+            </div>
+
+            <div className="FormTitle">
+              <a href="#" className="FormTitle__Link">Sign In</a> or
+              <a href="#" className="FormField__Link FormField__Link--Active">Sign Up</a>
+            </div>
+
+            <div className="FormCenter">
+              <form className="FormFields" onSubmit={this.handleSubmit}>
+                <div className="FormField">
+                  <label className="FormField__Label" htmlFor="name">Full Name</label>
+                  <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+                </div>
+                <div className="FormField">
+                  <label className="FormField__Label" htmlFor="password">Password</label>
+                  <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+                </div>
+                <div className="FormField">
+                  <label className="FormField__Label" htmlFor="email">Enter email</label>
+                  <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
+                </div>
+                <div className="FormField">
+                  <button className="FormField__Button mr-20">Sign Up</button>
+                  <a href="#" className="FormField__Link">Already a member!</a>
+                </div>
+              </form>
+            </div>
+        </div>
       </div>
     );
   }
