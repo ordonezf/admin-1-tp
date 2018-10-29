9725215cabd5e6b5abe972e86c47124d771e4d3e
Manuel Porto <manu.porto94@gmail.com>
2018-10-29 00:28:30 -0300

    Create SignIn component

---
+++ src/components/authentication/SignIn.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing.unit,
+    },
+    textField: {
+      flexBasis: 200,
+    },
+  });
+
+class SignIn extends React.Component {
+    state = {
+        name: '',
+        password: '',
+        showPassword: false,
+      };
+    
+    handleChange = prop => event => {
+        this.setState({ [prop]: event.target.value });
+    };
+
+    handleClickShowPassword = () => {
+        this.setState(state => ({ showPassword: !state.showPassword }));
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <TextField
+                    id="outlined-name"
+                    label="Name"
+                    className={classes.textField}
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-adornment-password"
+                    className={classNames(classes.margin, classes.textField)}
+                    variant="outlined"
+                    type={this.state.showPassword ? 'text' : 'password'}
+                    label="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="Toggle password visibility"
+                            onClick={this.handleClickShowPassword}
+                            >
+                            {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+            </div>
+        )
+    }
+}
+
+SignIn.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignIn);
