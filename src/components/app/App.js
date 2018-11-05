b4fef20846825ccbb429fcfcce4b02c519fc3036
Manuel Porto <manu.porto94@gmail.com>
2018-11-04 21:18:12 -0300

    Add new routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,8 @@ import NavBar from '../navbar/NavBar'
 import SignIn from '../authentication/SignIn'
 import SignUp from '../authentication/SignUp'
 import SearchTurns from '../turns/SearchTurns'
+import AppointmentList from '../controls/AppointmentList';
+import AppointmentForm from '../controls/AppointmentForm';
 
 class App extends React.Component {
     render() {
@@ -15,6 +17,8 @@ class App extends React.Component {
                     <Route path="/" exact component={SignIn} />
                     <Route path="/signup" component={SignUp} />
                     <Route path="/search" component={SearchTurns} />
+                    <Route path="/appointments" component={AppointmentList} />
+                    <Route path="/new-appointment" component={AppointmentForm} />
                 </div>
             </Router>
         )
