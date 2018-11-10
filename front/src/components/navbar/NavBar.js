01d0f2605ef14a6fea550ee09013ce65387b4077
Manuel Porto <manu.porto94@gmail.com>
2018-11-09 22:07:57 -0300

    Add simple menu drawer (WIP)

--- front/src/components/navbar/NavBar.js
+++ front/src/components/navbar/NavBar.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => {
+import MenuDrawer from './Menu'
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+};
+
+const NavBar = (props) => {
+    const { classes } = props;
     return(
         <div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
+                <MenuDrawer />
+                <Typography variant="h6" color="inherit" className={classes.grow}>
                 Mis Turnos Medicos
                 </Typography>
             </Toolbar>
@@ -17,4 +35,8 @@ const NavBar = () => {
     )
 }
 
-export default NavBar;
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(NavBar);
