ed0beccb0c8353bd64fad07a5b063dd60b3a7c7b
Manuel Porto <manu.porto94@gmail.com>
2018-11-11 22:13:51 -0300

    Minor change

--- front/src/components/privateRoute/PrivateRoute.js
+++ front/src/components/privateRoute/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom'
 
-const PrivateRoute = ({ component: Component, ...rest }, isAuthenticated) => (
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
     <Route {...rest} render={(props) => (
       isAuthenticated === true
         ? <Component {...props} />
