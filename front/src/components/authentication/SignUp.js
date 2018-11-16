c2c5b5cbfba5054363b0f92dc2819b3973bd0db6
Manuel Porto <manu.porto94@gmail.com>
2018-11-16 10:12:57 -0300

    Visual changes and error fixing

--- front/src/components/authentication/SignUp.js
+++ front/src/components/authentication/SignUp.js
@@ -9,21 +9,23 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { CardContent, CardActions, CardHeader } from '@material-ui/core';
+import { CardContent, CardActions, CardHeader, Snackbar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-const qs = require('querystring');
+import qs from 'querystring';
 
 const styles = theme => ({
     margin: {
       margin: theme.spacing.unit,
     },
     base : {
-        marginTop: '1em',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        'transform': 'translate(-50%, -50%)',
+        width: '50%',
+        margin: '1em auto',
+        // marginTop: '1em',
+        // position: 'fixed',
+        // top: '50%',
+        // left: '50%',
+        // 'transform': 'translate(-50%, -50%)',
     },
     textField: {
       flexBasis: 200,
@@ -38,9 +40,10 @@ class SignUp extends React.Component {
         lastName: '',
         birthday: '',
         mail: '',
-        username: '',
         password: '',
         showPassword: false,
+        registrationResultSnack: false,
+        registrationResult: 'Registro exitoso!',
       };
     
     handleChange = prop => event => {
@@ -53,114 +56,118 @@ class SignUp extends React.Component {
 
     handleRegister = async () => {
         console.log('Click happened');
-        console.log(this.state)
+        console.log(this.state);
         let results;
-        let url = 'http://localhost:5555/back/signup'
+        const url = 'http://localhost:5555/back/signup';
         try {
-            results = await axios.post(url, qs.stringify(this.state))
+            results = await axios.post(url, qs.stringify(this.state));
         } catch(err) {
-            console.log(err)
+            console.log(err);
         }
-        console.log(results)
+        console.log(results);
+        this.setState({ successfulRegistration: true});
     };
 
+    handleClose = () => {
+        this.setState({registrationResultSnack: false});
+    }
+    
     render() {
         const { classes } = this.props;
 
         const signInLink = props => <Link to="/" {...props} />
 
         return (
-            <Card className={classNames(classes.base)}>
-                <CardHeader title="Sign Up" />
-                <CardContent>
-                    <TextField
-                        id="username"
-                        label="Username"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.username}
-                        onChange={this.handleChange('username')}
-                        margin="normal"
-                    />
-                    <br />
-                    <TextField
-                        id="adornment-password"
-                        className={classNames(classes.margin, classes.textField)}
-                        type={this.state.showPassword ? 'text' : 'password'}
-                        label="Password"
-                        value={this.state.password}
-                        onChange={this.handleChange('password')}
-                        InputProps={{
-                            endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                aria-label="Toggle password visibility"
-                                onClick={this.handleClickShowPassword}
-                                >
-                                {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                            ),
-                        }}
-                    />
-                    <TextField
-                        id="dni"
-                        label="DNI"
-                        type="number"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.dni}
-                        onChange={this.handleChange('dni')}
-                        margin="normal"
-                    />
-                    <TextField
-                        id="first-name"
-                        label="First name"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.firstName}
-                        onChange={this.handleChange('firstName')}
-                        margin="normal"
-                    />
-                    <br />
-                    <TextField
-                        id="last-name"
-                        label="Last name"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.lastName}
-                        onChange={this.handleChange('lastName')}
-                        margin="normal"
-                    />
-                    <br />
-                    <TextField
-                        id="birthday"
-                        label="Birthday"
-                        type="date"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.birthday}
-                        onChange={this.handleChange('birthday')}
-                        margin="normal"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <br />
-                    <TextField
-                        id="mail"
-                        label="Mail"
-                        type="email"
-                        className={classNames(classes.margin, classes.textField)}
-                        value={this.state.mail}
-                        onChange={this.handleChange('mail')}
-                        margin="normal"
-                    />
-                </CardContent>
-                <CardActions>
-                    <Button variant="contained" color="primary" className={classes.margin} onClick={this.handleRegister}>
-                        Register
-                    </Button>
-                    <Button className={classes.margin} component={signInLink}>
-                        Already a member? Sign in
-                    </Button>
-                </CardActions>
-            </Card>
+            <div>
+                <Card className={classNames(classes.base)}>
+                    <CardHeader title="Sign Up" />
+                    <CardContent>
+                        <TextField
+                            id="dni"
+                            label="DNI"
+                            type="number"
+                            className={classNames(classes.margin, classes.textField)}
+                            value={this.state.dni}
+                            onChange={this.handleChange('dni')}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="first-name"
+                            label="Nombre"
+                            className={classNames(classes.margin, classes.textField)}
+                            value={this.state.firstName}
+                            onChange={this.handleChange('firstName')}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="last-name"
+                            label="Apellido"
+                            className={classNames(classes.margin, classes.textField)}
+                            value={this.state.lastName}
+                            onChange={this.handleChange('lastName')}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="adornment-password"
+                            className={classNames(classes.margin, classes.textField)}
+                            type={this.state.showPassword ? 'text' : 'password'}
+                            label="Constraseña"
+                            value={this.state.password}
+                            onChange={this.handleChange('password')}
+                            InputProps={{
+                                endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="Toggle password visibility"
+                                    onClick={this.handleClickShowPassword}
+                                    >
+                                    {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <TextField
+                            id="birthday"
+                            label="Cumpleaños"
+                            type="date"
+                            className={classNames(classes.margin, classes.textField)}
+                            value={this.state.birthday}
+                            onChange={this.handleChange('birthday')}
+                            margin="normal"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            id="mail"
+                            label="Mail"
+                            type="email"
+                            className={classNames(classes.margin, classes.textField)}
+                            value={this.state.mail}
+                            onChange={this.handleChange('mail')}
+                            margin="normal"
+                        />
+                    </CardContent>
+                    <CardActions>
+                        <Button variant="contained" color="primary" className={classes.margin} onClick={this.handleRegister}>
+                            Registrarse
+                        </Button>
+                        <Button className={classes.margin} component={signInLink}>
+                            ¿Ya está registrado? Ingresar
+                        </Button>
+                    </CardActions>
+                </Card>
+                <Snackbar 
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={this.state.registrationResultSnack}
+                autoHideDuration={6000}
+                onClose={this.handleClose} 
+                message={this.state.registrationResult} />
+            </div>
         )
     }
 }
