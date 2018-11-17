64b04a11a9372d6ae7d8a913d6592ddf6f79958c
Manuel Porto <manu.porto94@gmail.com>
2018-11-17 18:13:45 -0300

    Move 'server' responses to server connector

--- front/src/components/appointments/AppointmentForm.js
+++ front/src/components/appointments/AppointmentForm.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import { Card, CardContent, CardActions, CardHeader } from '@material-ui/core';
 
-
 const styles = theme => ({
-  base : {
+  base: {
     width: '50%',
     margin: '1em auto',
   },
@@ -30,105 +29,35 @@ const styles = theme => ({
   menu: {
     width: 200,
   },
-  datepicker:{
-      marginTop:2*theme.spacing.unit,
-      marginRight:theme.spacing.unit,
-      marginLeft:theme.spacing.unit
+  datepicker: {
+    marginTop: 2 * theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginLeft: theme.spacing.unit,
   },
 });
 
-const allspecialities = [
-  {
-    value: 'CRD',
-    label: 'Cardiology',
-  },
-  {
-     value: 'TRM',
-     label: 'Traumatology',
-  },
-  {
-    value: 'PSGY',
-    label: 'Plastic Surgery',
-  },
-  {
-    value: 'PDT',
-    label: 'Pediatrics',
-  },
-];
-
-const allphisicians = [
-  {
-    value: 1123,
-    label: 'Dr.Watson',
-    speciality: 'PDT'
-  },
-  {
-     value: 1822,
-     label: 'Dr.Strange',
-     speciality: 'TRM'
-
-  },
-  {
-    value: 1984,
-    label: 'Dr.Brown',
-    speciality: 'PSGY'
-  },
-];
-
-const someTakenAppExample = [
-    {date: "2018-11-12",hour:"10", minutes:"00"},
-    {date: "2018-11-12",hour:"13", minutes:"30"},
-    {date: "2018-11-12",time:"16", minutes:"30"}
-];
-
 class AppointmentForm extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            patientId: 0,
-            selectedSpeciality: allspecialities[0],
-            phisicians: allphisicians,
-            data: [],
-            excludedTimes:[],
-            dateTime: '',
-            // dateTime: moment().hours(5)
-        };
-
-        this.updateSearchDate = this.updateSearchDate.bind(this);
-    }
-
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      patientId: 0,
+      allspecialities: [],
+      selectedSpeciality: '',
+      phisicians: [],
     };
 
-    updateSearchDate(date) {
-        let taken = someTakenAppExample.map(
-            (datetime)=>{
-                // return moment(datetime.date,'YYYY-MM-DD').hours(Number.parseInt(datetime.hour)).minutes(Number.parseInt(datetime.minutes))
-                return '';
-            }
-        )
-        this.setState({
-            dateTime:date,
-            excludedTimes: taken
-        });
-    };
+    this.updateSearchDate = this.updateSearchDate.bind(this);
+  }
 
-    searchAppointments(event){
-        event.preventDefault();
-        alert("searching...")
-    };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
-    render() {
-        let filteredPhisicians = this.state.phisicians.filter(
-        (phisician) => {
-            return (
-                phisician.speciality === this.state.selectedSpeciality
-            )
-        }
-    );
+  getAppointment() {}
+
+  render() {
     const { classes } = this.props;
     return (
       <Card className={classes.base}>
@@ -146,7 +75,7 @@ class AppointmentForm extends React.Component {
             helperText="Por favor seleccione una especialidad"
             margin="normal"
           >
-            {allspecialities.map(option => (
+            {this.state.allspecialities.map(option => (
               <option key={option.value} value={option.value}>
                 {option.label}
               </option>
@@ -167,7 +96,7 @@ class AppointmentForm extends React.Component {
               shrink: true,
             }}
           >
-            {filteredPhisicians.map(option => (
+            {this.state.phisicians.map(option => (
               <option key={option.value} value={option.value}>
                 {option.label}
               </option>
@@ -178,28 +107,15 @@ class AppointmentForm extends React.Component {
           <Button
             variant="contained"
             color="primary"
-            onClick={this.searchAppointments}>
-              Sacar turno
+            onClick={this.getAppointment}
+          >
+            Sacar turno
           </Button>
         </CardActions>
-          {/* <DatePicker
-              minDate={moment()}
-              maxDate={moment().add(6, "months")}
-              showWeekNumbers
-              className={classes.datepicker}
-              selected={this.state.dateTime}
-              onChange={this.updateSearchDate}
-              showTimeSelect
-              minTime={moment().hours(9).minutes(0)}
-              maxTime={moment().hours(18).minutes(30)}
-              dateFormat="LLL"
-              excludeTimes={this.state.excludedTimes}
-          /> */}
       </Card>
     );
   }
 }
-
 
 AppointmentForm.propTypes = {
   classes: PropTypes.object.isRequired,
--- front/src/connectors/ServerConnector.js
+++ front/src/connectors/ServerConnector.js
@@ -19,6 +19,51 @@ const appointmentsInfo = {
     },
 };
 
+
+const allspecialities = [
+    {
+      value: 'CRD',
+      label: 'Cardiology',
+    },
+    {
+       value: 'TRM',
+       label: 'Traumatology',
+    },
+    {
+      value: 'PSGY',
+      label: 'Plastic Surgery',
+    },
+    {
+      value: 'PDT',
+      label: 'Pediatrics',
+    },
+  ];
+  
+const allphisicians = [
+{
+    value: 1123,
+    label: 'Dr.Watson',
+    speciality: 'PDT'
+},
+{
+    value: 1822,
+    label: 'Dr.Strange',
+    speciality: 'TRM'
+
+},
+{
+    value: 1984,
+    label: 'Dr.Brown',
+    speciality: 'PSGY'
+},
+];
+  
+const someTakenAppExample = [
+    {date: "2018-11-12",hour:"10", minutes:"00"},
+    {date: "2018-11-12",hour:"13", minutes:"30"},
+    {date: "2018-11-12",time:"16", minutes:"30"}
+];
+
 class ServerConnector {
     getAppointmentsInfo() {
         return appointmentsInfo;
